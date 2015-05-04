d0c583fe244c1ad044941237fe6d6e5dbebe16f3
gpace <giancarlopace148@gmail.com>
2015-05-04 17:37:43 +0100

    Create closeHtml.php

---
+++ closeHtml.php
@@ -0,0 +1,17 @@
+<body>
+<br>
+<br>
+<div class="footer">
+<img class="centralImage" alt="Developers" src="images/developers.png"></img>
+<br>
+<div class="centralImage">
+<a target="_blank" href="http://www.astro.up.pt/exoearths/"><img alt="Exo Earths" src="images/EXOEarths.png"></img></a>
+<a target="_blank" href="http://erc.europa.eu/"><img alt="European Research Council" src="images/logo_erc.png"></img></a>
+<a target="_blank" href="http://cordis.europa.eu/fp7/home_en.html"><img alt="Seventh Framework Programme" src="images/logo_fp7.png"></img></a>
+<a target="_blank" href="http://europa.eu/"><img alt="European Union" src="images/logo_eu.png"></img></a>
+<a target="_blank" href="http://www.iastro.pt/"><img alt="Instituto de Astrofísica" src="images/iaLogo.png"></img></a>
+<a target="_blank" href="http://alfa.fct.mctes.pt/"><img alt="Fundação para a Ciência e Tecnologia" src="images/logo_fct.png"></img></a>
+</div>
+</div>
+</body>
+</html>
