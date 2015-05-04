ef7b318b4aae66eb64eb021a80c34826598083cb
gpace <giancarlopace148@gmail.com>
2015-05-04 18:04:24 +0100

    Create openHtml.php

---
+++ openHtml.php
@@ -0,0 +1,13 @@
+<html>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+
+
+<head>
+<script src="http://www.astro.up.pt/js/jquery-1.5.2.min.js"></script>
+<hr class="divisor">
+<img  class="centralImage" src="images/header.jpg" alt="ULME">
+<h1>
+The <span class="ULMEfont">U</span>se of  <span class="ULMEfont">L</span>BLRTM 
+<span class="ULMEfont">M</span>ade <span class="ULMEfont">E</span>asy
+</h1>
+</head>
