ae1d2f6f4a369a28d24d33ab92f6722f013bc6e7
gpace <giancarlopace148@gmail.com>
2015-05-04 17:50:46 +0100

    Create downloadDataSet.php

---
+++ downloadDataSet.php
@@ -0,0 +1,19 @@
+<?php
+
+ob_start();
+
+
+
+;
+require 'functions.php';
+require 'openHtml.php';
+
+?>
+<div class="pHome">
+This is the page that will allow you to get from the web all the data for the xxth set of Records 
+</div>
+
+<?php
+
+require 'closeHtml.php';
+?>
